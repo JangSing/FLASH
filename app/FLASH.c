4569cd4f6eaa07cf4b13dc4bd68706b6bd027fc7
JangSing <jangsing94@live.com.my>
2015-12-06 18:21:14 +0800

    Found error source, the PSIZE should use x32 rather than using the x64

--- app/FLASH.c
+++ app/FLASH.c
@@ -5,15 +5,77 @@ void unlockFlashControl(){
   FLASH->KEYR =0xCDEF89AB;
 }
 
+void sectorErase(){
+
+	int busyCount=0;
+	uint32_t *readAdd;
+  
+	if(!checkBusy()){
+
+		checkSRnCR();
+		FLASH->CR &= ~(FLASH_CR_SER);
+		FLASH->CR &= ~(FLASH_CR_SNB4);
+		FLASH->CR &= ~(FLASH_CR_STRT);
+		checkSRnCR();
+		FLASH->CR |= FLASH_CR_SER;
+		FLASH->CR |= FLASH_CR_SNB4;
+		FLASH->CR |= FLASH_CR_STRT;
+		checkSRnCR();
+
+		while(checkBusy()){
+			readAdd=((uint32_t *)(0x08100000));
+			busyCount++;
+		}
+	}
+	FLASH->CR &= ~FLASH_CR_SER;
+	FLASH->CR &= ~FLASH_CR_SNB4;
+	checkSRnCR();
+}
+
+void flashProgram(){
+	int busyCount=0,error=0;
+	uint32_t *write,*readAdd;
+
+	write=((uint32_t *)(0x08100000));
+
+	if(!checkBusy()){
+
+		checkSRnCR();
+		FLASH->CR &= ~(FLASH_CR_PSIZE0);
+		FLASH->CR &= ~(FLASH_CR_PSIZE1);
+		checkSRnCR();
+		//FLASH->CR |= FLASH_CR_PSIZE0;
+		FLASH->CR |= FLASH_CR_PSIZE1;
+		checkSRnCR();
+		FLASH->CR &= ~(FLASH_CR_PG);
+		FLASH->CR |= FLASH_CR_PG;
+		checkSRnCR();
+
+		error=checkError();
+
+		checkSRnCR();
+
+		*write=0x01010101;
+		error=checkError();
+
+		while(checkBusy()){
+			readAdd=((uint32_t *)(0x08100000));
+			busyCount++;
+		}
+		readAdd=((uint32_t *)(0x08100000));
+	}
+
+
+}
+
 int checkBusy(){
-	return ((FLASH->SR<<16)&1);
+	return ((FLASH->SR>>16)&1);
 }
 
 int checkError(){
   int seqErr,parallelErr,alignErr;
-  uint32_t checkSR,checkCR;
-  checkSR=FLASH->SR;
-  checkCR=FLASH->CR;
+  
+  checkSRnCR();
   seqErr      = (FLASH->SR>>7)&1;
   parallelErr = (FLASH->SR>>6)&1;
   alignErr    = (FLASH->SR>>5)&1;
@@ -24,3 +86,10 @@ int checkError(){
     return 0;
 }
   
+void checkSRnCR(){
+	uint32_t checkSR,checkCR;
+	checkSR=FLASH->SR;
+	checkCR=FLASH->CR;
+}
+
+
--- app/main.c
+++ app/main.c
@@ -1,77 +1,31 @@
 #include "FLASH.h"
 #include <stdint.h>
 
-
-
-void sectorErase(){
-
-	int busyCount=0;
-	uint32_t checkSR,checkCR;
-	uint32_t *readAdd;
-	int busy=(FLASH->SR & SR_BSY)>>16;
-	if(!busy){
-		FLASH->CR |= CR_SER;
-		FLASH->CR |= CR_SNB4;
-		FLASH->CR |= CR_STRT;
-		checkSR=FLASH->SR;
-		checkCR=FLASH->CR;
-		busy=(FLASH->SR & SR_BSY)>>16;
-		while(busy){
-			readAdd=((uint32_t *)(0x08100000));
-			busy=(FLASH->SR & SR_BSY)>>16;
-			busyCount++;
-		}
-	}
-	FLASH->CR &= ~CR_SER;
-	FLASH->CR &= ~CR_SNB4;
-	checkSR=FLASH->SR;
-	checkCR=FLASH->CR;
-}
-
-void flashProgram(){
-	int busyCount=0,error=0;
-	uint32_t checkSR,checkCR;
-	uint32_t *write,*readAdd;
-
-	write=((uint32_t *)(0x08100000));
-	int busy=(FLASH->SR & SR_BSY)>>16;
-	if(!busy){
-
-		FLASH->CR |= CR_PSIZE0;
-		FLASH->CR |= CR_PSIZE1;
-		FLASH->CR |= CR_PG;
-		error=checkError();
-		checkSR=FLASH->SR;
-		checkCR=FLASH->CR;
-		*write=1;
-		busy=(FLASH->SR & SR_BSY)>>16;
-		error=checkError();
-		/*
-
-		while(busy){
-			readAdd=((uint32_t *)(0x08100000));
-			busy=(FLASH->SR & SR_BSY)>>16;
-			busyCount++;
-		}
-		readAdd=((uint32_t *)(0x08100000));
-		*/
-	}
-
-
-}
-
 int main(void)
 {
 	int i,error;
 	uint32_t checkCR;
-	uint32_t *readSector;
+	uint32_t *readAdd;
+	uint32_t guard=0,firstTime=1;
 
-	unlockFlashControl();
-	error=checkError();
-	sectorErase();
-	readSector=((uint32_t *)0x08100000);
-	flashProgram();
-
+	while(1){
+		if(firstTime){
+			unlockFlashControl();
+			firstTime=0;
+		}
+		error=checkError();
+		readAdd=((uint32_t *)0x08100000);
+		if(guard){
+			sectorErase();
+			guard=0;
+		}
+		readAdd=((uint32_t *)0x08100000);
+		if(guard){
+			flashProgram();
+			guard=0;
+		}
+		readAdd=((uint32_t *)0x08100000);
+	}
 
 
 
